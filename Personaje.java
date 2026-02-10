c291d2c53940b660936fd3f56a8d7f2e1d576093
Xnor <98614335+Xn0rC0d3@users.noreply.github.com>
2026-02-10 13:40:56 +0100

    Adjust potion recovery thresholds in Personaje.java

--- Personaje.java
+++ Personaje.java
@@ -93,17 +93,16 @@ public class Personaje {
         } else if (num >= 50 && num < 80) {
             this.setVidaActual(vidaActual + POCION_MINIMA);
             System.out.println("El monstruo dejó caer lo que parece ser una pequeña y rudimentaria poción de salud. Tras beberla, recuperas " + POCION_MINIMA + " puntos de vida.");
-        } else if (num >= 80 && num < 90) {
+        } else if (num >= 80 && num < 92) {
             this.setVidaActual(vidaActual + POCION_MEDIA);
             System.out.println("El monstruo dejó caer lo que parece un botellín de ambar que parece contener una poción de salud. Tras beberla, recuperas " + POCION_MEDIA + " puntos de salud.");
-        } else if (num >= 90 && num < 98) {
+        } else if (num >= 92 && num < 99) {
             setVidaActual(vidaActual + POCION_GRANDE);
             System.out.println("El monstruo dejó caer una especie de bota de vino llena de poción de salud. Tras beberla, recuperas " + POCION_GRANDE + " puntos de salud.");
-        } else if (num >= 98 && num < 100) {
+        } else if (num >= 99 && num < 100) {
             setVidaActual(vidaActual = vidaMaxima);
             System.out.println("El monstruo dejó caer un precioso frasco de cristal lleno de un líquido de movimiento hipnotizante." +
                     "Parece ser una poción de salud muy avanzada. Recuperas todos tus puntos de salud!");
-
         }
         if (this.vidaActual > this.vidaMaxima){ //la curación no puede exceder el máximo de vida
             this.vidaActual = this.vidaMaxima;
