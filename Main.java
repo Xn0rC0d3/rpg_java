da8b25f2738370247cc1c70bb0549a1fb1fcc7e4
Xnor <98614335+Xn0rC0d3@users.noreply.github.com>
2026-02-12 11:59:28 +0100

    Fix formatting and comments in Main.java

--- Main.java
+++ Main.java
@@ -28,7 +28,7 @@ public class Main {
 
     public static void main(String[] args) {
         Lore.printPrologo();
-        System.out.print("¿Cómo te llamas, héroe? : ");
+        System.out.print("¿Cómo te llamas, héroe?: ");
         nombreJugador = sc.nextLine();
         System.out.println("\n\n");
         crearPersonajes(nombreJugador); //crear personajes
@@ -49,38 +49,38 @@ public class Main {
             do {
                 System.out.println("---------------------------------");
                 System.out.println(nombreJugador + " ataca");
-                jugador.atacar(personajes[i]);
-                if(personajes[i].getVidaActual() > 0){
+                jugador.atacar(personajes[i]); //ataque del jugador
+                if(personajes[i].getVidaActual() > 0){//si el monstruo sobrevive se muestra su vida restante
                     System.out.println("Al monstruo le quedan " +  personajes[i].getVidaActual() + " puntos de vida.\n");
-                }else{
+                }else{//si no, combate ganado
                     break;
                 }
 
                 System.out.println(personajes[i].getNombre() + " contraataca");
-                personajes[i].atacar(jugador);
-                if(jugador.getVidaActual() > 0){
+                personajes[i].atacar(jugador);//contraataque del monstruo
+                if(jugador.getVidaActual() > 0){//si el jugador sobrevive se muestra su vida restante
                     System.out.println("Te quedan " + jugador.getVidaActual() + " puntos de vida");
                 }
                 System.out.println("---------------------------------");
 
-                if (!jugador.comprobarVida()) break;
+                if (!jugador.comprobarVida()) break;//si no, juego perdido
 
             } while (personajes[i].comprobarVida() || jugador.comprobarVida());
 
-            if (!jugador.comprobarVida()) {
-                victoria = false;
-                break;
+            if (!jugador.comprobarVida()) {//comprobar cuál ha sido la causa de salir del do-while
+                victoria = false;//si el jugador ha muerto, indicamos que el jugador ha perdido
+                break;//y salimos también del for
             }
-            //esto debe suceder lo último, o habría alguna posibilidad de "revivir"
-            jugador.curarTrasCadaCombate(); //tras derrotar a cada enemigo, existe una pequeña posibilidad de recuperar algo de salud
+            //tras derrotar a cada enemigo, existe una pequeña posibilidad de recuperar algo de salud
+            jugador.curarTrasCadaCombate(); //esto debe suceder lo último, o habría alguna posibilidad de "revivir"
         }
 
         System.out.println("\n\n");
-        if (victoria) {
+        if (victoria) {//si al llegar aquí esta variable sigue como true, el jugador se ha pasado el juego
             System.out.println("Has vencido a todos tus enemigos");
             System.out.println("Has finalizado el juego con " + jugador.getVidaActual() + " puntos de vida.");
             Arte.printVictoria();
-        } else {
+        } else {//si no, es que ha perdido
             Arte.printDerrota();
         }
     }
