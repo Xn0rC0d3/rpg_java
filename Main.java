3edfa5c6420643ce032f387ea12070cc8bbea12c
Xnor <98614335+Xn0rC0d3@users.noreply.github.com>
2026-02-12 13:28:35 +0100

    Refactor combat logic for improved readability

--- Main.java
+++ Main.java
@@ -49,38 +49,38 @@ public class Main {
             do {
                 System.out.println("---------------------------------");
                 System.out.println(nombreJugador + " ataca");
-                jugador.atacar(personajes[i]); //ataque del jugador
-                if(personajes[i].getVidaActual() > 0){//si el monstruo sobrevive se muestra su vida restante
+                jugador.atacar(personajes[i]);//ataque del jugador
+                if(personajes[i].comprobarVida()){ //si el monstruo sobrevive se muestra su vida restante
                     System.out.println("Al monstruo le quedan " +  personajes[i].getVidaActual() + " puntos de vida.\n");
-                }else{//si no, combate ganado
+                }else{ //si no, combate ganado
                     break;
                 }
 
                 System.out.println(personajes[i].getNombre() + " contraataca");
-                personajes[i].atacar(jugador);//contraataque del monstruo
-                if(jugador.getVidaActual() > 0){//si el jugador sobrevive se muestra su vida restante
+                personajes[i].atacar(jugador); //contraataque del monstruo
+                if(jugador.comprobarVida()){ //si el jugador sobrevive se muestra su vida restante
                     System.out.println("Te quedan " + jugador.getVidaActual() + " puntos de vida");
                 }
                 System.out.println("---------------------------------");
 
-                if (!jugador.comprobarVida()) break;//si no, juego perdido
+                if (!jugador.comprobarVida()) break; //si no, juego perdido
 
             } while (personajes[i].comprobarVida() || jugador.comprobarVida());
 
-            if (!jugador.comprobarVida()) {//comprobar cuál ha sido la causa de salir del do-while
-                victoria = false;//si el jugador ha muerto, indicamos que el jugador ha perdido
-                break;//y salimos también del for
+            if (!jugador.comprobarVida()) { //comprobar cuál ha sido la causa de salir del do-while
+                victoria = false; //si el jugador ha muerto, indicamos que el jugador ha perdido
+                break; //y salimos también del for
             }
             //tras derrotar a cada enemigo, existe una pequeña posibilidad de recuperar algo de salud
-            jugador.curarTrasCadaCombate(); //esto debe suceder lo último, o habría alguna posibilidad de "revivir"
-        }
+            jugador.curarTrasCadaCombate();//esto debe suceder lo último, o habría alguna posibilidad de "revivir"
+        } //fin del bucle for
 
         System.out.println("\n\n");
-        if (victoria) {//si al llegar aquí esta variable sigue como true, el jugador se ha pasado el juego
+        if (victoria) { //si al llegar aquí esta variable sigue como true, el jugador se ha pasado el juego
             System.out.println("Has vencido a todos tus enemigos");
             System.out.println("Has finalizado el juego con " + jugador.getVidaActual() + " puntos de vida.");
             Arte.printVictoria();
-        } else {//si no, es que ha perdido
+        } else { //si no, es que ha perdido
             Arte.printDerrota();
         }
     }
